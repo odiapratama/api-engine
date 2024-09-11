57967378acd2bcb59727879676f6a91027d062c2
Odia Pratama <odiapratama@gmail.com>
2024-09-11 15:38:53 +0700

    init project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const ui = require('./ui')
+
+app.get('/', (req, res) => {
+  res.send('Api is running')
+})
+
+app.get('/ui', (req, res) => {
+  res.json(ui)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running')
+})
