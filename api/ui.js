7149c29aaac62e9d807abae729e0c4b9acd35f9a
Odia Pratama <odiapratama@gmail.com>
2024-09-12 11:04:44 +0700

    update type to email

--- api/ui.js
+++ api/ui.js
@@ -15,7 +15,7 @@ module.exports = {
                     "id": 1,
                     "name": "username",
                     "label": "Username",
-                    "type": "text",
+                    "type": "email",
                     "placeholder": "Enter your username"
                 },
                 {
